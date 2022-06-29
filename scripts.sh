a909c18f475192c08d78536e9cd54ddc40cf382c
Serj <serj.by@yahoo.com>
2022-06-29 22:02:30 +0300

    Updated script and Cinnamon config

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,27 @@
+#!/bin/bash
+
+# Make sure password is known
+#sudo -k
+sudo echo "Logged in as $USER..."
 # Instant fnmode change
-echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode
+echo "Changing Fn keys to acts as Fn Kays by default for this session...".
+echo 2 | sudo tee /sys/module/hid_apple/parameters/fnmode && echo "Done."
 # Permanent fnmode change
-echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf
-sudo update-initramfs -u -k all
+echo "Changing Fn keys to acts as Fn Kays by default for permanently...".
+echo "options hid_apple fnmode=2" | sudo tee /etc/modprobe.d/hid_apple.conf && echo "Done"
+read -p "Update kernel (y/n)?" q;
+if [[ $q =~ ^[Yy]$ ]]; then
+    echo "Updating kernel...";
+    sudo update-initramfs -u -k all && echo "Done";
+else
+    echo "Skipping kernel update..."
+fi
+
+read -p "Restore all Cinnamon settings (y/n)?" q;
+if [[ $q =~ ^[Yy]$ ]]; then
+    echo "Cinnamon setting restoring...";
+    sudo dconf load /org/cinnamon/ < cin-all-dconf.txt && echo "Done. Press Ctrl+Alt+Esc to restart Cinnamon";
+else
+    echo "Skipping Cinnamon settings restore...";
+fi
+
