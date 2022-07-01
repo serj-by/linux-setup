075ac37b557a6a4da468b734f438af79016b7125
Serj <serj.by@yahoo.com>
2022-07-01 18:02:44 +0300

    Dumps backups automation

--- dumpconf.sh
+++ dumpconf.sh
@@ -1,2 +1,21 @@
-mv cin-all-dconf.txt oldconfs/cin-all-dconf-bak-`date +%y%m%d`.txt
-dconf dump /org/cinnamon/ > cin-all-dconf.txt
+dconf_set_name="cin-all-dconf.txt"
+orig_name="oldconfs/cin-all-dconf-bak-`date +%y%m%d`"
+file_name="$orig_name-01"
+cnt=1
+while [ -f "$file_name.txt" ]
+do
+  cnt=$((cnt+1))
+  if [[ $cnt -lt 10 ]]
+  then
+    file_name="$orig_name-0$cnt"
+  else
+    file_name="$orig_name-$cnt"
+  fi
+done
+file_name="$file_name.txt"
+echo "Writing backup to $file_name"
+mv cin-all-dconf.txt $file_name
+echo "Dumping settings..."
+dconf dump /org/cinnamon/ > $dconf_set_name
+echo "Diff with previous:"
+diff $file_name $dconf_set_name && echo "No differrence with latest backup"
